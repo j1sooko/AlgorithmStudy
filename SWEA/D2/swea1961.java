96b9a750def6a7e4c9718d98a9e6ced4ff0759de
jisoo ko <doubletry23@gmail.com>
2023-05-12 01:27:15 +0900

    solve : [SWEA] 숫자 배열 회전 / D2
    https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV5Pq-OKAVYDFAUq&categoryId=AV5Pq-OKAVYDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=2&pageSize=10&pageIndex=2

---
+++ SWEA/D2/swea1961.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class swea1961 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int[][] sudoku = new int[9][9];
+		int T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int ans = 1;
+			for (int i = 0; i < 9; i++) {
+				String[] input = br.readLine().split(" ");
+				for (int j = 0; j < 9; j++) {
+					// 입력 받기
+					sudoku[i][j] = Integer.parseInt(input[j]);
+				}
+			}
+
+			//가로 체크
+			for (int i = 0; i < 9; i++) {
+				boolean[] isUsed = new boolean[10];
+				for (int j = 0; j < 9; j++) {
+					if (isUsed[sudoku[i][j]]) {
+						ans = 0;
+						break;
+					} else {
+						isUsed[sudoku[i][j]] = true;
+					}
+				}
+			}
+			
+			//세로 체크
+			for (int i = 0; i < 9; i++) {
+				boolean[] isUsed = new boolean[10];
+				for (int j = 0; j < 9; j++) {
+					if (isUsed[sudoku[j][i]]) {
+						ans = 0;
+						break;
+					} else {
+						isUsed[sudoku[j][i]] = true;
+					}
+				}
+			}
+			
+			//격자 체크
+			for (int i = 0; i <= 6; i += 3) {
+				for (int j = 0; j <= 6; j += 3) {
+					boolean[] isUsed = new boolean[10];
+					for (int r = 0; r < 3; r++) {
+						for (int c = 0; c < 3; c++) {
+							if (isUsed[sudoku[i + r][j + c]]) {
+								ans = 0;
+								break;
+							} else {
+								isUsed[sudoku[i + r][j + c]] = true;
+							}
+						}
+					}
+				}
+			}
+			
+			sb.append("#" + test_case + " " + ans + "\n");
+		}
+
+		bw.write(sb.toString());
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
