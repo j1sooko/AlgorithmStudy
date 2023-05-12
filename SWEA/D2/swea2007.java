df5601015c2f6a5ef4aed0fdebe9d8978c08dc23
jisoo ko <doubletry23@gmail.com>
2023-05-13 00:44:40 +0900

    improve : [SWEA] 패턴 마디의 길이 / D2 /  'ababc' 같은 패턴의 경우 개선
    https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5P1kNKAl8DFAUq

--- SWEA/D2/swea2007.java
+++ SWEA/D2/swea2007.java
@@ -11,27 +11,19 @@ public class swea2007 {
 		int T = Integer.parseInt(br.readLine());
 		for (int test_case = 1; test_case <= T; test_case++) {
 			String input = br.readLine();
-			String tmp = "";
+			String pattern = "";
+			int patternLen;
 			for (int i = 0; i < 30; i++) {
-				// 다음 문자에 tmp와 동일한 문자열이 있는지 확인한다
-				boolean isSame = true;
-				for (int j = 0; j < tmp.length(); j++) {
-					if (input.charAt(i + j) != tmp.charAt(j)) {
-						isSame = false;
+				pattern += input.charAt(i);
+				patternLen = pattern.length();
+				if (pattern.equals(input.substring(patternLen, patternLen + patternLen))) {
+					//패턴을 제거하고 남은 남은 문자열 길이가 패턴 길이보다  작을 경우 break
+					String restString = input.replace(pattern, "");
+					if (restString.length() < patternLen)
 						break;
-					}
-				}
-				
-				// 있을 경우 tmp의 문자 길이 반환
-				if (isSame && !tmp.isEmpty()) {
-					break;
-				}
-				// 없을 경우 tmp에 현재 문자를 추가한다
-				else {
-					tmp += input.charAt(i);
 				}
 			}
-			System.out.println("#" + test_case + " " + tmp.length());
+			System.out.println("#" + test_case + " " + pattern.length());
 
 		}
 		br.close();
