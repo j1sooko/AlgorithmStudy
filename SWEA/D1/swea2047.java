12ef78354044c629a2895740400a7b87b16e9a1a
jisoo ko <doubletry23@gmail.com>
2023-05-12 20:06:46 +0900

    solve : [SWEA] 신문 헤드라인 / D1
    https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QKsLaAy0DFAUq&categoryId=AV5QKsLaAy0DFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=10&pageIndex=1

---
+++ SWEA/D1/swea2047.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class swea2047 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		String s = sc.nextLine();
+		
+		System.out.print(s.toUpperCase());
+		sc.close();
+	}
+
+}
