d2ab6a3671075e0b34bf72c273d1d006a5ebbe41
jisoo ko <doubletry23@gmail.com>
2023-05-16 20:26:44 +0900

    improve : [SWEA] 농작물 수확하기 / D3 / 스택 대신 반복문 사용
    https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV7GLXqKAWYDFAXB

--- SWEA/D3/swea2805.java
+++ SWEA/D3/swea2805.java
@@ -4,13 +4,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class swea2805 {
-	static int sum;
-	static int[][] farm;
-
-	static void solve(int N, int k, int a) {
-		for (int i = 0 + a; i < N - a; i++)
-			sum += farm[N / 2 + k][i];
-	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,22 +13,29 @@ public class swea2805 {
 		int T = Integer.parseInt(br.readLine());
 		for (int test_case = 1; test_case <= T; test_case++) {
 			int N = Integer.parseInt(br.readLine());
-			farm = new int[N][N];
+			int[][] farm = new int[N][N];
 
 			for (int i = 0; i < N; i++) {
-				String[] input = br.readLine().split("");
+				String input = br.readLine();
 				for (int j = 0; j < N; j++)
-					farm[i][j] = Integer.parseInt(input[j]);
+					farm[i][j] = input.charAt(j) - '0';
 			}
 
-			sum = 0;
+			int sum = 0;
 
-			solve(N, 0, 0);
-			for (int i = 1; i <= N / 2; i++) {
-				solve(N, -i, i);
-				solve(N, i, i);
+			for (int i = 1; i <= N / 2; i++) { // 마름모윗부분
+				for (int j = i; j < N - i; j++)
+					sum += farm[N / 2 - i][j];
 			}
-			// solve(N, 0, 0); // 0행부터 시작
+
+			for (int j = 0; j < N; j++) // 마름모가운데부분
+				sum += farm[N / 2][j];
+
+			for (int i = 1; i <= N / 2; i++) { // 마름모아랫부분
+				for (int j = i; j < N - i; j++)
+					sum += farm[N / 2 + i][j];
+			}
+
 			sb.append("#" + test_case + " " + sum + "\n");
 		}
 		bw.write(sb.toString());
