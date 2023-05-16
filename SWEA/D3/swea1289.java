693916eca86735d139b906c4fd62f820b2f96b7e
jisoo ko <doubletry23@gmail.com>
2023-05-16 23:26:28 +0900

    solve : [SWEA] 원재의 메모리 복구하기 / D3
    https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&contestProbId=AV19AcoKI9sCFAZN&categoryId=AV19AcoKI9sCFAZN&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=ALL&select-1=3&pageSize=10&pageIndex=1

---
+++ SWEA/D3/swea1289.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class swea1289 {
+	static int cnt;
+	static int[] board;
+
+	static void nQueen(int N, int k) {
+
+		if (k == N) {// N개의 말을 모두 놓았음
+			cnt++;
+		} else {
+			for (int i = 0; i < N; i++) { // 하나의 행마다 N개의 열씩 말을 놓아본다(가로 체크: 한 행에 1개를 놓으면 끝)
+				boolean isPossible = true;
+				for (int j = 0; j < k; j++) { // 이전에 놓았던 말들에 대해 세로, 오른쪽 대각선, 왼쪽 대각선 체크
+					if (i == board[j] || i == board[j] + (k - j) || i == board[j] - (k - j))
+						isPossible = false;
+				}
+				if (isPossible) {
+					board[k] = i;
+					nQueen(N, k + 1);
+				}
+			}
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = Integer.parseInt(br.readLine());
+			cnt = 0;
+			board = new int[N];
+			nQueen(N, 0); // 0행부터 시작
+			sb.append("#" + test_case + " " + cnt + "\n");
+		}
+		bw.write(sb.toString());
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
