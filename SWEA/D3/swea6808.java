62678ac4bd04f4e584f4240cd40c24984403671f
jisoo ko <doubletry23@gmail.com>
2023-05-19 16:28:54 +0900

    improve : [SWEA] 규영이와 인영이의 카드게임 / D3 / 시간 초과 해결
    https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&contestProbId=AWgv9va6HnkDFAW0&categoryId=AWgv9va6HnkDFAW0&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=ALL&select-1=3&pageSize=10&pageIndex=3

--- SWEA/D3/swea6808.java
+++ SWEA/D3/swea6808.java
@@ -5,49 +5,34 @@ import java.io.OutputStreamWriter;
 
 public class swea6808 {
 	static int winCnt, loseCnt;
-	static int[] card1;
-	
-	static void judge(int[] card2) {
-		int s1 = 0, s2 = 0;
-		for (int i = 0; i < card1.length; i++) {
-			if (card1[i] > card2[i])
-				s1 += card1[i] + card2[i];
-			else
-				s2 += card1[i] + card2[i];
-		}
-		
-		if (s1 > s2)
-			winCnt++;
-		else if (s1 < s2)
-			loseCnt++;
-	}
+	static int[] card1, card2, item;
 
-	static void pick(int[] item, int[] bucket, int k) { // bucket: 카드의 인덱스를 뽑는 배열, k: 뽑아야하는 카드 수
-		int lastIndex = bucket.length - k - 1; // bucket에 뽑은 마지막 index
+	static void pick(boolean[] isSelected, int k) { // isSelected: 인덱스에 해당하는 카드를 뽑았는지의 여부, k: 뽑은 카드수
 		int smallest = 0; // 만약 조합일 경우 작은 것부터 뽑게함(bucket[lastIndex] + 1 : 전에 뽑은 것보다 큰 수
 
-		if (k == 0) { // 모든 카드를 뽑음
-			int[] card2 = new int[9];
-			for (int i = 0; i < card2.length; i++)
-				card2[i] = item[bucket[i]];
-			judge(card2);
+		if (k == 9) { // 모든 카드를 뽑음
+			int s1 = 0, s2 = 0;
+			for (int i = 0; i < card1.length; i++) {
+				if (card1[i] > card2[i])
+					s1 += card1[i] + card2[i];
+				else if (card1[i] < card2[i])
+					s2 += card1[i] + card2[i];
+			}
+			
+			if (s1 > s2)
+				winCnt++;
+			else if (s1 < s2)
+				loseCnt++;
 			return;
 		}
 
-		for (int i = smallest; i < item.length; i++) {
-			boolean chosen = false;
-			for (int j = 0; j <= lastIndex; j++) // 이전에 그 카드를 뽑은 적이 있음
-				if (bucket[j] == i) {
-					chosen = true;
-					break;
-				}
-
-			if (!chosen) { // 이전에 뽑은 적이 카드일 경우 뽑는다
-				bucket[lastIndex + 1] = i;
-				pick(item, bucket, k - 1);
+		for (int i = smallest; i < 9; i++)
+			if (!isSelected[i]) {
+				card2[k] = item[i];
+				isSelected[i] = true;
+				pick(isSelected, k + 1);
+				isSelected[i] = false; // 다른 경우의 수에서 다시 뽑을 수 있도록
 			}
-		}
-
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -59,31 +44,28 @@ public class swea6808 {
 		for (int test_case = 1; test_case <= T; test_case++) {
 			winCnt = 0;
 			loseCnt = 0;
+			card1 = new int[9]; //규영이 낸 카드
+			card2 = new int[9]; //인영이 낼 카드
+			item = new int[9]; //인영이 가지고 있는 카드
 			
 			// 규영의 카드 입력 받기
 			String[] input = br.readLine().split(" ");
-			card1 = new int[9];
-			for (int i = 0; i < card1.length; i++)
+			boolean[] used = new boolean[19];
+			for (int i = 0; i < card1.length; i++) {
 				card1[i] = Integer.parseInt(input[i]);
-			
-			
-			// 인영의 카드 배열 만들기
-			int[] item = new int[9];
-			int i = 0;
-			for (int n = 1; n <= 18; n++) {
-				boolean chosen = false;
-				for (int j = 0; j < card1.length; j++)
-					if (card1[j] == n) {
- 						chosen = true;
- 						break;
-					}
-				if (!chosen)
-					item[i++] = n;
+				used[card1[i]] = true;
 			}
+			
+			
+			// 인영의 가지고 있는 카드 배열 만들기
+			int i = 0;
+			for (int n = 1; n <= 18; n++)
+				if (!used[n])
+					item[i++] = n;
 
 			// 카드를 내는 경우의수 -> 순열
-			int[] bucket = new int[9];
-			pick(item, bucket, 9);
+			boolean[] isSelected = new boolean[9];
+			pick(isSelected, 0);
 
 			sb.append("#" + test_case + " " + winCnt + " " + loseCnt + "\n");
 		}
