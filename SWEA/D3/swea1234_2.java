672198f7b383dafd01d482ad13d980b45dbcdf14
jisoo ko <doubletry23@gmail.com>
2023-05-19 09:18:09 +0900

    solve: [SWEA] [S/W 문제해결 기본] 10일차 - 비밀번호 / D3 / 스택을 이용해서 풀기
    https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14_DEKAJcCFAYD

---
+++ SWEA/D3/swea1234_2.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class swea1234_2 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			String[] input = br.readLine().split(" ");
+			int N = Integer.parseInt(input[0]); // 문자열 길이
+			ArrayList<String> arr = new ArrayList<>(Arrays.asList(input[1].split("")));
+	
+			int i  = 0;
+			while (i < arr.size() - 1) {
+				if (arr.get(i).equals(arr.get(i + 1))) {
+					arr.remove(i);
+					arr.remove(i);
+					i = 0;
+				} else
+					i++;
+			}
+			
+
+			sb.append("#" + test_case + " ");
+			for (String s : arr)
+				sb.append(s);
+			sb.append("\n");
+		}
+		bw.write(sb.toString());
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
